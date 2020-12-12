8f16874954d526b87b97bdeaa1397e099065d49e
杨金 <jin.yang@qlchat.com>
2020-12-12 15:09:26 +0800

    top100

---
+++ algorithm/src/com/yangjin/leetCode/top100/A_2.java
@@ -0,0 +1,86 @@
+package com.yangjin.leetCode.top100;
+
+import com.yangjin.leetCode.ListNode;
+
+/**
+ * 2. 两数相加
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，
+ * 并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class A_2 {
+    public static void main(String[] args) {
+        A_2 a2 = new A_2();
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(4);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(5);
+        ListNode node5 = new ListNode(6);
+        ListNode node6 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node4.next = node5;
+        node5.next = node6;
+        System.out.println(a2.addTwoNumbers(node1, node4));
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = null;
+        ListNode prev = null;
+        int more = 0;
+        while (true) {
+            int val1 = 0;
+            if (l1 != null) {
+                val1 = l1.val;
+                l1 = l1.next;
+            }
+            int val2 = 0;
+            if (l2 != null) {
+                val2 = l2.val;
+                l2 = l2.next;
+            }
+            int val = val1 + val2 + more;
+            if (val >= 10) {
+                val = val % 10;
+                more = 1;
+            } else {
+                more = 0;
+            }
+            ListNode listNode = new ListNode(val);
+            if (head == null) {
+                head = listNode;
+            }
+            if (prev != null) {
+                prev.next = listNode;
+            }
+            prev = listNode;
+//            System.out.println(listNode.val);
+            if (more ==0 && l1 == null && l2 == null) {
+                break;
+            }
+        }
+        return head;
+    }
+}
